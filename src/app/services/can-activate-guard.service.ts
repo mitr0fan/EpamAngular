74d4be5a78d4ce00bc8f3798434603c1b228df02
mitr0fan <antontoxa.ru@gmail.com>
2020-11-10 14:11:11 +0300

    fixed bug

--- src/app/services/can-activate-guard.service.ts
+++ src/app/services/can-activate-guard.service.ts
@@ -2,25 +2,25 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthorizationService } from './authorization.service';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { User } from '../user';
+import { Store } from '@ngrx/store';
+import { selectUserStatus } from 'src/store/selectors/users.selector';
 
 @Injectable({
     providedIn: 'root',
 })
 export class CanActivateGuardService implements CanActivate {
-    constructor(private authService: AuthorizationService, private router: Router) {}
+    constructor(private store: Store, private router: Router) {}
 
     canActivate(): Observable<boolean> {
-        return this.authService.getUserInfo().pipe(
-            map((user: User) => {
-                if (user) {
-                    return true;
-                } else {
-                    this.router.navigate(['/login']);
-                    return false;
-                }
-            })
-        );
+        return this.store.select(selectUserStatus)
+            .pipe(
+                tap(s => {
+                    if (!s) {
+                        this.router.navigate(['/login']);
+                    }
+                })
+            );
     }
 }
